c1fb4b77aa37157a3eeb872ffdcab45c5c56bdd7
Claret15 <37543659+Claret15@users.noreply.github.com>
2019-04-01 22:06:37 +0100

    Re-configure handlebars and add partials

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
-const createError     = require('http-errors');
-const express         = require('express');
-const path            = require('path');
-const cookieParser    = require('cookie-parser');
-const logger          = require('morgan');
-const exphbs          = require('express-handlebars');
-const debug           = require('debug')('ninjas-node:server');
-const helmet          = require('helmet');
+const createError       = require('http-errors');
+const express           = require('express');
+const path              = require('path');
+const cookieParser      = require('cookie-parser');
+const logger            = require('morgan');
+const exphbs            = require('express-handlebars');
+const debug             = require('debug')('ninjas-node:server');
+const helmet            = require('helmet');
 
-const indexRouter     = require('./routes/index');
-const guildsRouter     = require('./routes/guild');
-const membersRouter   = require('./routes/members');
-const leaguesRouter   = require('./routes/league');
-const eventTypesRouter   = require('./routes/event_types');
-const eventsRouter   = require('./routes/events');
+const indexRouter       = require('./routes/index');
+const guildsRouter      = require('./routes/guild');
+const membersRouter     = require('./routes/members');
+const leaguesRouter     = require('./routes/league');
+const eventTypesRouter  = require('./routes/event_types');
+const eventsRouter      = require('./routes/events');
 
 const app = express();
 
@@ -26,8 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
 
 // Routes
 // define default routes and managed by routers/controllers
@@ -39,12 +42,12 @@ app.use('/api/event_types', eventTypesRouter);
 app.use('/api/events', eventsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message || err;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
