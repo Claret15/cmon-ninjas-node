b27e567b3d50ea0f11baa7f55df5a46b3bd5f98a
Claret15 <37543659+Claret15@users.noreply.github.com>
2019-04-09 12:09:40 +0100

    Update home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'Chris'});
+  res.render('index', { title: 'Ninjas API'});
 });
 
 module.exports = router;
